38cb344bcef32b5a4f2b710016cdfd4ca140aec5
Jamie Houston <jamie.houston@salesforce.com>
2021-08-10 11:30:11 -0500

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id ("org.sonarqube") version "3.3"
+    id("org.sonarqube") version "3.3"
 }
-
 
 allprojects {
     repositories {
@@ -23,6 +22,9 @@ allprojects {
 sonarqube {
     properties {
         property("sonar.host.url", "http://localhost:9000")
-        property ("sonar.coverage.jacoco.xmlReportPaths", "${rootProject.projectDir}/code-coverage-report/build/reports/jacoco/codeCoverageReport/codeCoverageReport.xml")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${rootProject.projectDir}/code-coverage-report/build/reports/jacoco/codeCoverageReport/codeCoverageReport.xml"
+        )
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,8 @@
 rootProject.name = "jvm-multi-project-with-code-coverage"
 
 // production code projects
-include("application", "list", "utilities", "androidmodule")
+//include("application", "list", "utilities", "androidmodule")
+include("application", "list", "utilities")
 
 // reporting utility projects
 include("code-coverage-report")
